230926524c3949ecc52609df0af2b4e7bd8e9a74
Fozimat <fozimata@gmail.com>
2021-06-20 10:51:06 +0700

    Initial commit

---
+++ app/src/main/java/com/fozimat/roomcrud/data/User.kt
@@ -0,0 +1,13 @@
+package com.fozimat.roomcrud.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_user")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
---
+++ app/src/main/java/com/fozimat/roomcrud/data/UserDao.kt
@@ -0,0 +1,17 @@
+package com.fozimat.roomcrud.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: User)
+
+    @Query("SELECT * FROM tb_user ORDER BY id ASC")
+    fun readAllData(): LiveData<List<User>>
+}
